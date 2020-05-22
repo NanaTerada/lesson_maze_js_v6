61dae67842339b3f05fa85ebf60140df692e6acd
n2 <7worki.nprogress@gmail.com>
2020-05-22 19:33:57 +0900

    #06 スタート､ゴールを設定しよう

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 
 (function () {
-console.log('test')
     //手順1.迷路を配列で用意
 
 //     0 0 0
@@ -19,15 +18,24 @@ console.log('test')
     var col = 3;
     var row = 3;
 
+
+    // 手順6.スタートとゴール
+    var startX = 0;
+    var startY = 0;
+    var goalX = col - 1;
+    var goalY = row - 1;
+
     var wallSize = 10;
     var wallColor = '#3261AB';
+
+    var routeColor = '#FF0088'
 
     var canvas = document.getElementById('mycanvas');
 
     if (!canvas || !canvas.getContext) {
         return false;
     }
-    console.log(canvas.getContext)
+    // console.log(canvas.getContext)
 
     var ctx = canvas.getContext('2d');
 
@@ -50,20 +58,44 @@ console.log('test')
     for (var x = 0; x < col ;x++) {
         for (var y = 0; y <row ;y++) {
             if (map[x][y] === 1) {
-                drawWall(x+1,y+1);
+                drawWall( x + 1, y+  1);
+            }
+            //手順6.スタートとゴールを描画
+            if ((x === startX && y === startY) || (x === goalX && y === goalY)) {
+
+                    drawRoute(x + 1,y + 1);
+                }
             }
         }
-    }
 
 
-    // 手順3.壁を描画する
-    function drawWall(x,y) {
-        ctx.fillStyle = wallColor;
+    
+
+
+
+    function drawRect(x,y){
         ctx.fillRect(
             x * wallSize,
             y * wallSize,
             wallSize,
             wallSize);
     }
+
+
+    // 手順3.壁を描画する
+    function drawWall(x,y) {
+        ctx.fillStyle = wallColor;
+        drawRect(x,y);
+    }
+
+    //手順6.スタートとゴールを描画
+
+    function drawRoute(x,y) {
+        ctx.fillStyle = routeColor;
+        drawRect(x,y);
+        
+    }
+ 
+
 
 })();
