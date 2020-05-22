561043160afa1d11dba64ce5a680e9bb1e036982
n2 <7worki.nprogress@gmail.com>
2020-05-22 17:41:40 +0900

    #05 迷路を描画してみよう

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
+
 (function () {
-    //迷路を配列で用意
+console.log('test')
+    //手順1.迷路を配列で用意
 
 //     0 0 0
 //     0 1 0
@@ -12,7 +14,7 @@
     map[1] = [0,1,1];
     map[2] = [0,0,0];
 
-    //Canvasで描画
+    //手順2.Canvasで描画
 
     var col = 3;
     var row = 3;
@@ -21,18 +23,47 @@
     var wallColor = '#3261AB';
 
     var canvas = document.getElementById('mycanvas');
+
     if (!canvas || !canvas.getContext) {
-        return;
+        return false;
     }
+    console.log(canvas.getContext)
 
     var ctx = canvas.getContext('2d');
 
-    canvas.width = (col + 2).wallSize;
-    canvas.width = (row + 2).wallSize;
+    canvas.width = (col + 2) * wallSize;
+    canvas.height = (row + 2) *wallSize;
 
-    // 上下の壁
+    // 手順4.上下の壁
+    for (var x = 0;x <col + 2; x++) {
+        drawWall(x,0);
+        drawWall(x,row + 1);
+    }
 
-    // 左右の壁
+    // 手順4.左右の壁
+    for (var y = 0;y <col + 2; y++) {
+        drawWall(0,y);
+        drawWall(col + 1,y);
+    }
 
+    //手順5.壁の内部
+    for (var x = 0; x < col ;x++) {
+        for (var y = 0; y <row ;y++) {
+            if (map[x][y] === 1) {
+                drawWall(x+1,y+1);
+            }
+        }
+    }
+
+
+    // 手順3.壁を描画する
+    function drawWall(x,y) {
+        ctx.fillStyle = wallColor;
+        ctx.fillRect(
+            x * wallSize,
+            y * wallSize,
+            wallSize,
+            wallSize);
+    }
 
 })();
