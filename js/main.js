4be0a1d576c7463b5b444960b0b5a1833eff6c3b
n2 <7worki.nprogress@gmail.com>
2020-05-22 16:50:42 +0900

    #04 Canvasの設定をしていこう

--- js/main.js
+++ js/main.js
@@ -14,4 +14,25 @@
 
     //Canvasで描画
 
+    var col = 3;
+    var row = 3;
+
+    var wallSize = 10;
+    var wallColor = '#3261AB';
+
+    var canvas = document.getElementById('mycanvas');
+    if (!canvas || !canvas.getContext) {
+        return;
+    }
+
+    var ctx = canvas.getContext('2d');
+
+    canvas.width = (col + 2).wallSize;
+    canvas.width = (row + 2).wallSize;
+
+    // 上下の壁
+
+    // 左右の壁
+
+
 })();
